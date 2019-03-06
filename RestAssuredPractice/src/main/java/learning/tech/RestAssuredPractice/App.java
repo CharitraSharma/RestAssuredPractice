86ee71cf5b1b17e1dd88880dacd78a34ca235e8d
charitrasharma <charitrakumarsharma@gmail.com>
2019-03-07 00:43:34 +0530

    Added Project

---
+++ RestAssuredPractice/src/main/java/learning/tech/RestAssuredPractice/App.java
@@ -0,0 +1,14 @@
+package learning.tech.RestAssuredPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Test Line");
+    }
+}
